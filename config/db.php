1e8b2ec41702df12fe05c7090b9e0543bfdca8a5
Novikov <novikov.sergey.nsk@gmail.com>
2015-12-19 14:31:04 +0600

    2gis test app init

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5433;dbname=test_database',
+    'username' => 'www',
+    'password' => 'kvnuajbq',
+    'charset' => 'utf8',
+];
