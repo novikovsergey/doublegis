f87566cc0b54073aad7721cd06cdf02a15444646
Novikov <novikov.sergey.nsk@gmail.com>
2015-12-22 03:07:25 +0600

    add Readme

--- README.md
+++ README.md
@@ -1,101 +1,155 @@
-Yii 2 Basic Project Template
-============================
+# Тестовое задание PHP разработчика
+## О проекте
+Постарался выполнить в соотвествии с заданием.
 
-Yii 2 Basic Project Template is a skeleton [Yii 2](http://www.yiiframework.com/) application best for
-rapidly creating small projects.
+Возможно не учёл требования по производительности, понятие 1000000 пользователей размытое.
+ Для проекта использовал Yii2, т.к. осваиваю его в рамках саморазвития. БД Postgres 9.4. Не использовал какого либо вида кеширование, в рамках тестого заданяи считаю избыточным. Тестировал на глаз.
 
-The template contains the basic features including user login/logout and a contact page.
-It includes all commonly used configurations that would allow you to focus on adding new
-features to your application.
+Сейчас приложение крутится на домене badcoder.ru, тачка домашняя Core 2 Duo 2.66, 6Gb ОЗУ, Debian 7
 
-[![Latest Stable Version](https://poser.pugx.org/yiisoft/yii2-app-basic/v/stable.png)](https://packagist.org/packages/yiisoft/yii2-app-basic)
-[![Total Downloads](https://poser.pugx.org/yiisoft/yii2-app-basic/downloads.png)](https://packagist.org/packages/yiisoft/yii2-app-basic)
-[![Build Status](https://travis-ci.org/yiisoft/yii2-app-basic.svg?branch=master)](https://travis-ci.org/yiisoft/yii2-app-basic)
+Админка badcoder.ru/admin
 
-DIRECTORY STRUCTURE
--------------------
+- login: admin
+- password: KZTgCD!--!_-Epx_
 
-      assets/             contains assets definition
-      commands/           contains console commands (controllers)
-      config/             contains application configurations
-      controllers/        contains Web controller classes
-      mail/               contains view files for e-mails
-      models/             contains model classes
-      runtime/            contains files generated during runtime
-      tests/              contains various tests for the basic application
-      vendor/             contains dependent 3rd-party packages
-      views/              contains view files for the Web application
-      web/                contains the entry script and Web resources
+## Требования к системе
+- Apache2
+- php 5.5
+- Postgres 9.4
+- Postgres Extension PostGIS
+- Postgres Extension Gist
+- composer
+- git
+## Установка
+```sh
+git clone https://github.com/novikovsergey/doublegis.git
+composer install
+```
+После настроить подключение к БД, затем:
+```sh
+ ./yii migrate
+ ./yii generator/generate
+```
+Генератор тестовых данных на больших обьёмах работае очень медленно, т.к. я ограничился ActiveRecord, по хорошему надо передлать, на массовые вставки данных в БД.
+## Методы
+# 1. buildings
+Метод выводит список всех зданий
+#### Параметры 
+не имеет
+#### Описание ответа
+список обьектов здания
 
+#### Поля:
+- id - идентификатор БД
+- address - строка с адресом
+- location - координаты на плоскости в формате "x,y"
+#### Пример запроса
+- badcoder.ru/buildings
+#### Пример ответа
+```json
+{
+	id: 1,
+	address: "821295, Калужская область, город Клин, ул. Славы, 20",
+	location: "-95.175643,82.44479"
+}
+```
+# 2. rubrics
+Метод возвращает всех родителей для заданных потомков, если параметры не указаны дерево возвращается целиком
+#### Параметры
 
+- ids - Идентификаторы вершин перечисленные через запятую  для которых вернуть родителей, по умолчанию не задан
+#### Описание ответа
+ список обьектов, произвольной вложенности
 
-REQUIREMENTS
-------------
+- id - идентификатор 
+- title - строка название
+- parent_id - идентификатор родителя
+- subrubrics - массив вложенных обьектов
 
-The minimum requirement by this project template that your Web server supports PHP 5.4.0.
+#### Пример запроса
+- badcoder.ru/rubrics?ids=10,25
 
+#### Пример ответа
+```json
+{
+	id: 10,
+	title: "tenetur",
+	parent_id: null,
+	subrubrics: [ ]
+},
+{
+	id: 11,
+	title: "deleniti",
+	parent_id: null,
+	subrubrics: [
+		{
+		id: 20,
+		title: "aut",
+		parent_id: 11,
+		subrubrics: [
+			{
+			id: 25,
+			title: "tempora",
+			parent_id: 20,
+			subrubrics: [ ]
+			}
+		]
+		}
+	]
+}
+```
+# 3. companies
+Метод возвращает список компаний подходящих запросу, ограничение вывода 1000 компаний
+#### Параметры
+- ids - Идентификаторы необходимых компаний перечисленные через запятую 
+- building_id - Идентификатор здания в котором находятся компании
+- rubric_ids -  Идентификаторы  перечисленные через запятую рубрик к которым относятся компании, компании  выводятся для всех наследников указанных рубрик
+- radius - Параметр описывает окружность в формате "x,y,R" в которую попадают компании
+- envelope - Параметр описывает прямоугольную область в формать "x_min,y_min,x_max,y_max" в которую попадают компании.
+- q - Строка поиска по названию компании
 
-INSTALLATION
-------------
+> Внимание параметры radius и envelope взаимоисключающие!
+#### Описание ответа
+список обьектов компаний
 
-### Install from an Archive File
+- id - идентификатор БД
+- title - строка название компании
+- address - адресс здания в котором находится компания
+- phone - массив с телефонами компании
+- rubrics - массив обьектов рубрик
+#### Пример запроса
+- badcoder.ru/companies?ids=10,25
+- badcoder.ru/companies?rubric_ids=18
+- badcoder.ru/companies?radius=10,-5,34.5&q=Текстиль
+- badcoder.ru/companies?envelope=10,-5,14.5,13.768346&rubric_ids=78,3
+- badcoder.ru/companies?building_id=10&q=гараж
 
-Extract the archive file downloaded from [yiiframework.com](http://www.yiiframework.com/download/) to
-a directory named `basic` that is directly under the Web root.
-
-Set cookie validation key in `config/web.php` file to some random secret string:
-
-```php
-'request' => [
-    // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-    'cookieValidationKey' => '<secret random string goes here>',
-],
+#### Пример ответа
+```json
+{
+	id: 6199,
+	title: "ЗАО ЦементТяжХоз",
+	address: "582927, Амурская область, город Одинцово, бульвар Будапештсткая, 08",
+	phones: [
+		"(812) 810-49-02",
+		"+7 (922) 574-9010",
+		"(812) 535-86-79",
+		"(35222) 69-2934",
+		"8-800-840-8476"
+	],
+	rubrics: [
+		{
+			id: 714,
+			title: "Quae"
+		},
+		{
+			id: 383,
+			title: "Tempore"
+		}
+	]
+},
 ```
 
-You can then access the application through the following URL:
-
-~~~
-http://localhost/basic/web/
-~~~
-
-
-### Install via Composer
-
-If you do not have [Composer](http://getcomposer.org/), you may install it by following the instructions
-at [getcomposer.org](http://getcomposer.org/doc/00-intro.md#installation-nix).
-
-You can then install this project template using the following command:
-
-~~~
-php composer.phar global require "fxp/composer-asset-plugin:~1.0.0"
-php composer.phar create-project --prefer-dist --stability=dev yiisoft/yii2-app-basic basic
-~~~
-
-Now you should be able to access the application through the following URL, assuming `basic` is the directory
-directly under the Web root.
-
-~~~
-http://localhost/basic/web/
-~~~
-
-
-CONFIGURATION
--------------
-
-### Database
-
-Edit the file `config/db.php` with real data, for example:
-
-```php
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8',
-];
-```
-
-**NOTE:** Yii won't create the database for you, this has to be done manually before you can access it.
-
-Also check and edit the other files in the `config/` directory to customize your application.
+## Коды ошибок методов
+- 200 - Успешный запроса
+- 400 - Забпрос составлен неверно
--- views/site/index.php
+++ views/site/index.php
@@ -4,50 +4,164 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-</div>
+<h1><a id="__PHP__0"></a>Тестовое задание PHP разработчика</h1>
+<h2><a id="__1"></a>О проекте</h2>
+<p><a href="https://github.com/novikovsergey/doublegis">Проект на GitHub (Там Readme выглядит лучше)</a></p>
+<p>Постарался выполнить в соотвествии с заданием.</p>
+<p>Возможно не учёл требования по производительности, понятие 1000000 пользователей размытое.
+    Для проекта использовал Yii2, т.к. осваиваю его в рамках саморазвития. БД Postgres 9.4. Не использовал какого либо вида кеширование, в рамках тестого заданяи считаю избыточным. Тестировал на глаз.</p>
+<p>Сейчас приложение крутится на домене <a href="http://badcoder.ru">badcoder.ru</a>, тачка домашняя Core 2 Duo 2.66, 6Gb ОЗУ, Debian 7</p>
+<p>Админка badcoder.ru/admin</p>
+<ul>
+    <li>login: admin</li>
+    <li>password: KZTgCD!–!-Epx</li>
+    </ul>
+<h2><a id="___9"></a>Требования к системе</h2>
+<ul>
+    <li>Apache2</li>
+    <li>php 5.5</li>
+    <li>Postgres 9.4</li>
+    <li>Postgres Extension PostGIS</li>
+    <li>Postgres Extension Gist</li>
+    <li>composer</li>
+    <li>git</li>
+</ul>
+<h2><a id="_17"></a>Установка</h2>
+<pre><code class="language-sh">git <span class="hljs-built_in">clone</span> https://github.com/novikovsergey/doublegis.git
+        composer install
+    </code></pre>
+<p>После настроить подключение к БД, затем:</p>
+<pre><code class="language-sh"> ./yii migrate
+        ./yii generator/generate
+    </code></pre>
+<p>Генератор тестовых данных на больших обьёмах работае очень медленно, т.к. я ограничился ActiveRecord, по хорошему надо передлать, на массовые вставки данных в БД.</p>
+<h2><a id="_28"></a>Методы</h2>
+<h1><a id="1_buildings_29"></a>1. buildings</h1>
+<p>Метод выводит список всех зданий</p>
+<h4><a id="_31"></a>Параметры</h4>
+<p>не имеет</p>
+<h4><a id="__33"></a>Описание ответа</h4>
+<p>список обьектов здания</p>
+<h4><a id="_36"></a>Поля:</h4>
+<ul>
+    <li>id - идентификатор БД</li>
+    <li>address - строка с адресом</li>
+    <li>location - координаты на плоскости в формате “x,y”</li>
+</ul>
+<h4><a id="__40"></a>Пример запроса</h4>
+<ul>
+    <li><a href="http://badcoder.ru/buildings">badcoder.ru/buildings</a></li>
+</ul>
+<h4><a id="__42"></a>Пример ответа</h4>
+<pre><code class="language-json">{
+        id: 1,
+        address: "821295, Калужская область, город Клин, ул. Славы, 20",
+        location: "-95.175643,82.44479"
+        }
+    </code></pre>
+<h1><a id="2_rubrics_50"></a>2. rubrics</h1>
+<p>Метод возвращает всех родителей для заданных потомков, если параметры не указаны дерево возвращается целиком</p>
+<h4><a id="_52"></a>Параметры</h4>
+<ul>
+    <li>ids - Идентификаторы вершин перечисленные через запятую  для которых вернуть родителей, по умолчанию не задан</li>
+</ul>
+<h4><a id="__55"></a>Описание ответа</h4>
+<p>список обьектов, произвольной вложенности</p>
+<ul>
+    <li>id - идентификатор</li>
+    <li>title - строка название</li>
+    <li>parent_id - идентификатор родителя</li>
+    <li>subrubrics - массив вложенных обьектов</li>
+</ul>
+<h4><a id="__63"></a>Пример запроса</h4>
+<ul>
+    <li><a href="http://badcoder.ru/rubrics?ids=10,25">badcoder.ru/rubrics?ids=10,25</a></li>
+</ul>
+<h4><a id="__66"></a>Пример ответа</h4>
+<pre><code class="language-json">{
+        id: 10,
+        title: "tenetur",
+        parent_id: null,
+        subrubrics: [ ]
+        },
+        {
+        id: 11,
+        title: "deleniti",
+        parent_id: null,
+        subrubrics: [
+        {
+        id: 20,
+        title: "aut",
+        parent_id: 11,
+        subrubrics: [
+        {
+        id: 25,
+        title: "tempora",
+        parent_id: 20,
+        subrubrics: [ ]
+        }
+        ]
+        }
+        ]
+        }
+    </code></pre>
+<h1><a id="3_companies_95"></a>3. companies</h1>
+<p>Метод возвращает список компаний подходящих запросу, ограничение вывода 1000 компаний</p>
+<h4><a id="_97"></a>Параметры</h4>
+<ul>
+    <li>ids - Идентификаторы необходимых компаний перечисленные через запятую</li>
+    <li>building_id - Идентификатор здания в котором находятся компании</li>
+    <li>rubric_ids -  Идентификаторы  перечисленные через запятую рубрик к которым относятся компании, компании  выводятся для всех наследников указанных рубрик</li>
+    <li>radius - Параметр описывает окружность в формате “x,y,R” в которую попадают компании</li>
+    <li>envelope - Параметр описывает прямоугольную область в формать “x_min,y_min,x_max,y_max” в которую попадают компании.</li>
+    <li>q - Строка поиска по названию компании</li>
+</ul>
+<blockquote>
+    <p>Внимание параметры radius и envelope взаимоисключающие!</p>
+</blockquote>
+<h4><a id="__106"></a>Описание ответа</h4>
+<p>список обьектов компаний</p>
+<ul>
+    <li>id - идентификатор БД</li>
+    <li>title - строка название компании</li>
+    <li>address - адресс здания в котором находится компания</li>
+    <li>phone - массив с телефонами компании</li>
+    <li>rubrics - массив обьектов рубрик</li>
+</ul>
+<h4><a id="__114"></a>Пример запроса</h4>
+<ul>
+    <li><a href="http://badcoder.ru/companies?ids=10,25">badcoder.ru/companies?ids=10,25</a></li>
+    <li><a href="http://badcoder.ru/companies?rubric_ids=18">badcoder.ru/companies?rubric_ids=18</a></li>
+    <li><a href="http://badcoder.ru/companies?radius=10,-5,34.5&amp;q=%D0%A2%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C">badcoder.ru/companies?radius=10,-5,34.5&amp;q=Текстиль</a></li>
+    <li><a href="http://badcoder.ru/companies?envelope=10,-5,14.5,13.768346&amp;rubric_ids=78,3">badcoder.ru/companies?envelope=10,-5,14.5,13.768346&amp;rubric_ids=78,3</a></li>
+    <li><a href="http://badcoder.ru/companies?building_id=10&amp;q=%D0%B3%D0%B0%D1%80%D0%B0%D0%B6">badcoder.ru/companies?building_id=10&amp;q=гараж</a></li>
+</ul>
+<h4><a id="__121"></a>Пример ответа</h4>
+<pre><code class="language-json">{
+        id: 6199,
+        title: "ЗАО ЦементТяжХоз",
+        address: "582927, Амурская область, город Одинцово, бульвар Будапештсткая, 08",
+        phones: [
+        "(812) 810-49-02",
+        "+7 (922) 574-9010",
+        "(812) 535-86-79",
+        "(35222) 69-2934",
+        "8-800-840-8476"
+        ],
+        rubrics: [
+        {
+        id: 714,
+        title: "Quae"
+        },
+        {
+        id: 383,
+        title: "Tempore"
+        }
+        ]
+        },
+    </code></pre>
+<h2><a id="___147"></a>Коды ошибок методов</h2>
+<ul>
+    <li>200 - Успешный запроса</li>
+    <li>400 - Забпрос составлен неверно</li>
+</ul>
